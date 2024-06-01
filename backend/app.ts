4279f1a1b37eba1828d75ebaeaf6cfaa1f1cdc17
dawa sherpa <sherpadawag1998@gmail.com>
2024-06-01 22:08:28 +0545

    feat: refresh token added  when access token expires

--- backend/app.ts
+++ backend/app.ts
@@ -5,6 +5,18 @@ import cookieParser from 'cookie-parser';
 import userRoute from './routes/user.routes'
 import { DBConnection } from './utils/database';
 import morgan from 'morgan';
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//cloudianry configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 
 //middleware;
--- backend/routes/user.routes.ts
+++ backend/routes/user.routes.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { activateUser, getUserInfo, userLogin, userRegister } from '../controller/user.controller';
 import { authentication } from '../middleware/authentication';
+import { refreshToken } from '../utils/refreshToken';
 
 const userRoute = express.Router();
 
+//post request
 userRoute.post('/register', userRegister)
 userRoute.post('/active-user', activateUser)
 userRoute.post('/login', userLogin)
+
+//get request
+userRoute.get('/refresh', refreshToken)
 userRoute.get('/user-info', authentication, getUserInfo)
 
 export default userRoute;
